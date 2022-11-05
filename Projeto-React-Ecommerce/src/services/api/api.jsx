22a865449f3c7759f7e8ccbbd72e171db844b51a
BragaMPedro <pedrobmagalhaes95@gmail.com>
2022-11-04 23:11:14 -0300

    corrige Api URL

--- Projeto-React-Ecommerce/src/services/api/api.jsx
+++ Projeto-React-Ecommerce/src/services/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL:"https://localhost:8080",
+    baseURL:"http://localhost:8080",
     headers: {
         'Content-Type': 'application/json'
     }
